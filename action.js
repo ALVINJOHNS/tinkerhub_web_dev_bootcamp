95f8eed7ec6cfbf9a0c2ccee92f6686f50d89ccc
ALVINJOHNS <98416925+ALVINJOHNS@users.noreply.github.com>
2022-02-02 01:53:52 -0800

    Add files via upload

---
+++ action.js
@@ -0,0 +1,27 @@
+function f1(){
+    document.body.style.backgroundColor="red"
+}
+function f2(){
+    document.body.style.backgroundColor="blue"
+}
+function f3(){
+    document.body.style.backgroundColor="green"
+}
+function f4(){
+    document.body.style.backgroundColor="yellow"
+}
+function f5(){
+    document.body.style.backgroundColor="violet"
+}
+function f6(){
+    document.body.style.backgroundColor="orange"
+}
+function f7(){
+    document.body.style.backgroundColor="aqua"
+}
+function f8(){
+    document.body.style.backgroundColor="rgb(82, 197, 140)"
+}
+function f9(){
+    document.body.style.backgroundColor="black"
+}
